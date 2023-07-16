c80475115232084309c9f5e70562ad62b23af32f
Márk Mezei <94128834+markmezei@users.noreply.github.com>
2023-07-16 17:16:53 +0200

    Add files via upload

---
+++ Never_gonna_give_you_up.ino
@@ -0,0 +1,16 @@
+#include "DigiKeyboard.h"
+
+void setup(){
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.delay(2000);
+    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+    DigiKeyboard.delay(100);
+    DigiKeyboard.println("powershell.exe");
+    DigiKeyboard.delay(1000);
+    DigiKeyboard.println("Start-Process -FilePath *location of brave.exe* ArgumentList 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'");
+}
+void loop(){
+
+
+  
+}
