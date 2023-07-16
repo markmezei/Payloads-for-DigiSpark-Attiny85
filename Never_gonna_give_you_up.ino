8790d6c241f4c329ac26f133d808f3c0fe630c5d
Márk Mezei <94128834+markmezei@users.noreply.github.com>
2023-07-16 17:39:47 +0200

    Update Never_gonna_give_you_up.ino

--- Never_gonna_give_you_up.ino
+++ Never_gonna_give_you_up.ino
@@ -7,7 +7,7 @@ void setup(){
     DigiKeyboard.delay(100);
     DigiKeyboard.println("powershell.exe");
     DigiKeyboard.delay(1000);
-    DigiKeyboard.println("Start-Process -FilePath *location of brave.exe* ArgumentList 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'");
+    DigiKeyboard.println("Start-Process -FilePath *location of your browser's executable file* ArgumentList 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'");
 }
 void loop(){
 
